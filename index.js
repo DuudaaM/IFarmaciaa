d67fcb2b72fae4f0f66cd04b1415d7ed1daa877c
Maria Eduarda <maria.camara@academico.ifpb.edu.br>
2021-03-19 00:25:39 +0000

    parte2

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ async function sincronizar(){
 }
 
 async function inserir(){
-Farmaceutico.create({nome:"Carla"})
-Farmaceutico.create({email:})
-Farmaceutico.create({senha:})
+Farmaceutico.create({nome:"Carla"}),
+Farmaceutico.create({email:"fulano00@gmail"}),
+Farmaceutico.create({senha:"1234ffx"})
 }
 
 async function consultar(){
@@ -30,7 +30,7 @@ async function sincronizar(){
 }
 
 async function inserir(){
-Farmacia.create(receita)
+Farmacia.create(receita:"xxxx durante y dias, z vezes ao dia")
 }
 
 async function consultar(){
@@ -51,16 +51,67 @@ async function sincronizar(){
 }
 
 async function inserir(){
-Medicamento.create({nome:})
-Medicamento.create({quantidade:}
-Medicamento.create({nome:}))}
+Medicamento.create({nome:"Dipirona"}),
+Medicamento.create({quantidade:7})
+}
 
 async function consultar(){
-  const farmaceutico = Farmaceutico.findAll();
-  console.log(farmaceutico)
+  const medicamento = Medicamento.findAll();
+  console.log(medicamento)
 }
 
 sincronizar();
 inserir();
 consultar();
+
+const Sequilize = require("sequilize");
+const db = require("./db");
+const Medico = require("./model/Medico")
+
+async function sincronizar(){
+  await db.sync();
+}
+
+async function inserir(){
+Medico.create({nome:"Carlos"}),
+Medico.create({crm:34578790}),
+Medico.create({email:"cicrano11@gmail.com"}),
+Medico.create({senha:"546763ggx"})
+}
+
+async function consultar(){
+  const medico = Medico.findAll();
+  console.log(medico)
+}
+
+sincronizar();
+inserir();
+consultar();
+
+
+const Sequilize = require("sequilize");
+const db = require("./db");
+const Paciente = require("./model/Paciente")
+
+async function sincronizar(){
+  await db.sync();
+}
+
+async function inserir(){
+Paciente.create({nome:"Clara"})
+Paciente.create({numero_SUS:2343616000})
+Paciente.create({receita:"xxxxx durante y dias, z vezes ao dia"})
+
+}
+
+async function consultar(){
+  const paciente = Paciente.findAll();
+  console.log(paciente)
+}
+
+sincronizar();
+inserir();
+consultar();
+
+
 
